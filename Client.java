d5825f636c7c51cd813581c53271a556fc118e29
Alhadj Nassourou <alhadjnassourou@gmail.com>
2026-04-28 19:12:11 +0100

    Rajout de l'attribut telephone et des getteurs et setteurs

--- Client.java
+++ Client.java
@@ -1,14 +1,26 @@
 public class Client {
 
     private String nom;
+    private String telephone;
 
-    public Client(String nom) {
+    public Client(String nom, String telephone) {
+        this.nom = nom;
+        this.telephone = telephone;
     }
 
     public String getNom() {
-        return null;
+        return nom;
+    }
+
+    public String getTelephone() {
+        return telephone;
     }
 
     public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 }
