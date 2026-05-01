1688a7ec0971da74e596fd14fb0faa230d38e480
Zenos237 <bigzen2511@gmail.com>
2026-05-01 18:47:40 +0100

    Ajout de StockService et Produit - Ajiago

--- Produit.java
+++ Produit.java
@@ -1,40 +1,31 @@
 public class Produit {
-
     private int id;
     private String nom;
     private double prix;
     private int quantite;
 
+    // Constructeur
     public Produit(int id, String nom, double prix, int quantite) {
+        this.id = id;
+        this.nom = nom;
+        this.prix = prix;
+        this.quantite = quantite;
     }
 
-    public int getId() {
-        return 0;
-    }
+    // Getters
+    public int getId() { return id; }
+    public String getNom() { return nom; }
+    public double getPrix() { return prix; }
+    public int getQuantite() { return quantite; }
 
-    public String getNom() {
-        return null;
-    }
+    // Setters
+    public void setNom(String nom) { this.nom = nom; }
+    public void setPrix(double prix) { this.prix = prix; }
+    public void setQuantite(int quantite) { this.quantite = quantite; }
 
-    public double getPrix() {
-        return 0;
-    }
-
-    public int getQuantite() {
-        return 0;
-    }
-
-    public void setNom(String nom) {
-    }
-
-    public void setPrix(double prix) {
-    }
-
-    public void setQuantite(int quantite) {
-    }
-
+    // Affichage
     @Override
     public String toString() {
-        return "";
+        return id + " | " + nom + " | " + prix + " € | Stock : " + quantite;
     }
 }
